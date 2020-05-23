187719a16b65348460b1075aa7d46467f4427f7f
ge-n <>
2020-05-23 20:38:57 +0900

    add round gimmick

--- assets/framework/components/gimmicks/ECRoundDetailEventData.ts
+++ assets/framework/components/gimmicks/ECRoundDetailEventData.ts
@@ -1,8 +1,86 @@
 import ECDetailEventData from "./ECDetailEventData";
+import ECRoundItem from "./ECRoundItem";
 
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class ECRoundDetailEventData extends ECDetailEventData {
+export default class ECRoundDetailEventData extends ECDetailEventData 
+{
+    @property([ECRoundItem]) items:ECRoundItem[] = [];
+    @property([cc.Integer]) passwords:number[] = [];
+    private excuting:boolean;
 
+    onInitialize()
+    {
+        this.node.on(cc.Node.EventType.TOUCH_START,this.onTouchStart,this);
+    }
+
+    onTouchStart(touch)
+    {
+        if(this.isCompleted) return;
+        
+		for (let i=0;i<this.items.length;i++)
+		{
+			let item:ECRoundItem = this.items[i];
+
+			if(item != null)
+			{
+				const world = (item.getComponent(cc.PolygonCollider) as any).world;
+				if (cc.Intersection.pointInPolygon(touch.getLocation(), world.points))
+				{
+					this.clickRound(item);
+					return;
+				}
+			}
+		}
+    }
+    
+    onDestroy()
+    {
+        this.node.off(cc.Node.EventType.TOUCH_START,this.onTouchStart,this);
+    }
+
+    public clickRound(item:ECRoundItem)
+    {
+        if(!this.excuting)
+        {
+            this.excuting = true;
+        
+            let index:number = item.index;
+            index++;
+            if(index >= item.angles.length)
+            {
+                index = 0;
+            }
+            item.index = index;
+            cc.tween(item.node).to(0.5,{angle:-item.angles[item.index]}).call(()=>{this.onRoteted(item)}).start();
+        }
+    }
+
+    onRoteted(item:ECRoundItem)
+    {
+        this.excuting = false;
+        if(item.index == item.angles.length - 1)
+        {
+            item.index = 0;
+            item.node.angle = 0;
+        }
+        this.check();
+        if(this.isCompleted)
+        {
+            this.unlock(false);
+        }
+    }
+
+    check()
+    {
+        for(let i = 0;i<this.passwords.length;i++)
+        {
+            if(this.passwords[i] != this.items[i].index)
+            {
+                return;
+            }
+        }
+        this.isCompleted = true;
+    }
 }
--- assets/framework/components/gimmicks/ECRoundItem.ts
+++ assets/framework/components/gimmicks/ECRoundItem.ts
@@ -3,5 +3,6 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class ECRoundItem extends cc.Component 
 {
-
+    @property([cc.Integer]) angles:number[] = [0,45,90,135,180,225,270,315,360];
+    @property index:number = 0;
 }
