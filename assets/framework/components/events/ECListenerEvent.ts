1a34bed1758f2ff69b0cf3f9061d784bd86e28b3
ge-n <>
2020-05-29 17:23:12 +0900

    eclistenerevent update

--- assets/framework/components/events/ECListenerEvent.ts
+++ assets/framework/components/events/ECListenerEvent.ts
@@ -3,11 +3,17 @@ import ECEvent from "./ECEvent";
 
 const {ccclass, property} = cc._decorator;
 
+const ECListenerBehaviors = cc.Enum({
+    PanelHid: 1,
+    UpdateFlag: 2
+});
+
 @ccclass
 export default class ECListenerEvent extends ECEvent 
 {
-    @property behavior:string = "";
+    @property({type:ECListenerBehaviors}) behavior = ECListenerBehaviors.PanelHid;
     @property param:string = "";
+    private flags:string[]=[];
 
     onInitialize()
     {
@@ -23,9 +29,13 @@ export default class ECListenerEvent extends ECEvent
 
         if(status == ECFlagStatus.Start || status == ECFlagStatus.Stay)
         {
-            if(this.behavior == ECEvents.PanelHid)
+            if(this.behavior == ECListenerBehaviors.PanelHid)
             {
-                cc.systemEvent.on(this.behavior,this.onPanelHid.bind(this),this);
+                cc.systemEvent.on(ECEvents.PanelHid,this.onPanelHid.bind(this),this);
+            }
+            else if(this.behavior == ECListenerBehaviors.UpdateFlag)
+            {
+                this.flags = this.param.split(",");
             }
         }
 
@@ -42,6 +52,29 @@ export default class ECListenerEvent extends ECEvent
         if(panel == this.param)
         {
             this.setFlagStatus(this.flag, ECFlagStatus.Complete);
+        }
+    }
+
+    onUpdateStatus(flag)
+    {
+        super.onUpdateStatus(flag);
+
+        if(flag != this.flag)
+        {
+            let allComplete = true;
+            for(let i=0; i < this.flags.length; i++)
+            {
+                let status = this.getFlagStatus(this.flags[i]);
+                if(status != ECFlagStatus.Complete || status != ECFlagStatus.CompleteHidden)
+                {
+                    allComplete = false;
+                }
+            }
+
+            if(allComplete)
+            {
+                this.setFlagStatus(this.flag, ECFlagStatus.Complete);
+            }
         }
     }
 }
